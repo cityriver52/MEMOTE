7d55d72968ab3802c367801542c030be077cd218
cityriver52 <cityriver52@gmail.com>
2026-09-08 16:45:20 +0900

    Log fatal startup failures

--- src/memoNOW/App.xaml.cs
+++ src/memoNOW/App.xaml.cs
@@ -1,3 +1,7 @@
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Threading;
+
 namespace memoNOW;
 
 public partial class App : System.Windows.Application
@@ -6,9 +10,67 @@ public partial class App : System.Windows.Application
 
     protected override void OnStartup(System.Windows.StartupEventArgs e)
     {
-        base.OnStartup(e);
+        HookDiagnostics();
+        DiagnosticLog.Write($"START memoNOW; OS={Environment.OSVersion}; 64bitOS={Environment.Is64BitOperatingSystem}; 64bitProcess={Environment.Is64BitProcess}; runtime={RuntimeInformation.FrameworkDescription}");
 
-        _mainWindow = new MainWindow();
-        _mainWindow.Show();
+        try
+        {
+            base.OnStartup(e);
+            DiagnosticLog.Write("Application startup entered.");
+
+            _mainWindow = new MainWindow();
+            DiagnosticLog.Write("MainWindow constructed.");
+
+            _mainWindow.Show();
+            DiagnosticLog.Write("MainWindow.Show completed.");
+        }
+        catch (Exception ex)
+        {
+            DiagnosticLog.WriteException("fatal startup", ex);
+            ShowFatalStartupMessage();
+            Shutdown(-1);
+        }
+    }
+
+    private void HookDiagnostics()
+    {
+        DispatcherUnhandledException += OnDispatcherUnhandledException;
+        AppDomain.CurrentDomain.UnhandledException += (_, args) =>
+        {
+            if (args.ExceptionObject is Exception ex)
+            {
+                DiagnosticLog.WriteException("AppDomain unhandled exception", ex);
+            }
+            else
+            {
+                DiagnosticLog.Write($"ERROR AppDomain unhandled exception: {args.ExceptionObject}");
+            }
+        };
+
+        TaskScheduler.UnobservedTaskException += (_, args) =>
+        {
+            DiagnosticLog.WriteException("unobserved task exception", args.Exception);
+        };
+    }
+
+    private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        DiagnosticLog.WriteException("dispatcher unhandled exception", e.Exception);
+        e.Handled = false;
+    }
+
+    private static void ShowFatalStartupMessage()
+    {
+        try
+        {
+            System.Windows.MessageBox.Show(
+                $"memoNOW の起動中にエラーが発生しました。\n\n診断ログ:\n{DiagnosticLog.CurrentLogPath}",
+                "memoNOW 起動エラー",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+        catch
+        {
+        }
     }
 }
